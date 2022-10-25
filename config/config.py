ed8a1a087faa9d965aea402dace7c335712789c9
ANONYMOUS <108213299+GOD-OF-LOVE@users.noreply.github.com>
2022-10-26 00:36:17 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = getenv("LOG_GROUP_ID", "-1001765414409")
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+MUSIC_BOT_NAME = getenv("heart of love")
 
 # Your User ID.
 OWNER_ID = list(
